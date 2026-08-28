3fdc774153602197315f5e103dccc89b1f88d4a5
rwpolar <sanskung3@gmail.com>
2026-08-28 15:22:01 +0700

    HP -15

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class Tree : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private MeshRenderer rd;
+
     void Start()
     {
-        
+        rd = GetComponent<MeshRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+            rd.material.color = Color.red;
+            Player player = collision.gameObject.GetComponent<Player>();
+            if (player == null) 
+                return;
+
+        player.Hp -= 15;
+
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        rd.material.color = new Color32(104, 50, 50, 255);
     }
 }
